c64b2e94a09eec284664f51d0396966072d82296
cynthiachinenye <cynthiachiagu@gmail.com>
2024-02-28 00:06:37 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// import  Brand from  './Component/Pages/Brand'
+// import SideBar from './Component/Sidebar/SideBar';
+// function App() {
+//   return (
+   
+//       <Router>
+//       <Brand/>
+//       <SideBar/>
+    
+//       <Routes>
+  
+//       </Routes>
+//       </Router>
+   
+//   );
+// }
+
+// export default App;
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import  Brand from  './Component/Pages/Brand'
-import SideBar from './Component/Sidebar/SideBar';
+import './App.css';
+import  Navbar from  './Component/Pages/Brand'
+ import SideBar from './Component/Sidebar/SideBar';
+
 function App() {
   return (
-   
-      <Router>
-      <Brand/>
-      <SideBar/>
-    
-      <Routes>
-  
-      </Routes>
-      </Router>
-   
+    <div className="App">
+      <SideBar />
+      <div className="content">
+        <Navbar />
+        <h1>Main Content</h1>
+      </div>
+    </div>
   );
 }
 
--- src/Component/Pages/Brand.jsx
+++ src/Component/Pages/Brand.jsx
@@ -1,23 +1,54 @@
-import React from 'react'
-import { Container} from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
+// import React from 'react'
+// import { Container} from 'react-bootstrap';
+// import { Navbar } from 'react-bootstrap';
 
 
-const Brand = () => {
-  return (
-    <div>
-      <Navbar className='bg-body-tertiary'>
-      <Container>
-      <Navbar.Toggle/>
-      <Navbar.Collapse className='justify-content-end'>
+// const Brand = () => {
+//   return (
+//     <div>
+//       <Navbar className='bg-body-tertiary'>
+//       <Container>
+//       <Navbar.Toggle/>
+//       <Navbar.Collapse className='justify-content-end'>
       
       
     
-      </Navbar.Collapse>
-      </Container>
-      </Navbar>
-    </div>
-  )
+//       </Navbar.Collapse>
+//       </Container>
+//       </Navbar>
+//     </div>
+//   )
+// }
+
+// export default Brand
+
+// Navbar.js
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Brand
+export default Navbar;
