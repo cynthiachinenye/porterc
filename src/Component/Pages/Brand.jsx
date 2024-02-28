db75e9859fb897f0ff275da82347c0437b146f47
cynthiachinenye <cynthiachiagu@gmail.com>
2024-02-29 00:42:34 +0100

    new

--- src/Component/Pages/Brand.jsx
+++ src/Component/Pages/Brand.jsx
@@ -1,26 +1,18 @@
-
+import '../../Styles/navbar.css'
 import React from 'react';
+import { VscBellDot } from "react-icons/vsc";
+
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light border-bottom  h-500px">
       <div className="container">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand " href="#">Dashboard</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
+          <VscBellDot className='bell-icon'/>
         </div>
       </div>
     </nav>
--- src/Component/Sidebar/SideBar.jsx
+++ src/Component/Sidebar/SideBar.jsx
@@ -1,40 +1,4 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-// import { RiHome2Line, RiSettings5Line } from "react-icons/ri";
-// import { TbMessage2 } from "react-icons/tb";
-// import { CiSearch } from "react-icons/ci";
-// import { FaRegFileLines } from "react-icons/fa6";
-// import { PiBuildings } from "react-icons/pi";
-// import { IoPersonOutline } from "react-icons/io5";
-// import { BiHelpCircle } from "react-icons/bi";
-
-// import '../../Styles/sidebar.css'
-// import Logo from '../../Assets/site_logo_1600674075 1.png'
-// const SideBar = () => {
-//   return (
-//     <div className='sidebar'>
-//     <img src={Logo} alt='logo'/>
-//     <div className='sidebar-content'>
-//     <Link to='/dashboard'><RiHome2Line/>Dashboard</Link>
-//     <Link to='/messages'><TbMessage2/>Messages</Link>
-//     <Link to='/applied'><FaRegFileLines/>Applied Jobs</Link>
-//     <Link to='/Find '><CiSearch/>Find Jobs</Link>
-//     <Link to='/companies'><PiBuildings />Browse Companies</Link>
-//     <Link to='/profile'><IoPersonOutline/>My Public Profile</Link>
-//     <p>SETTINGS</p>
-//     <Link to='settings'><RiSettings5Line/>Settings</Link>
-//     <Link to='/help '><BiHelpCircle/>Help Center</Link>
-//     </div>
-
-
-//       <div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default SideBar
-
+import "../../Styles/sidebar.css"
 import React from 'react'
 import Logo from '../../Assets/site_logo_1600674075 1.png'
 import { Link } from 'react-router-dom'
